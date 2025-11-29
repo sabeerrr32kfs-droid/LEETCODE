2752eb51437eb9e5e59a2b8648c5762368418212
sabeerrr32kfs-droid <sabeerrr32kfs@gmail.com>
2025-11-29 18:43:03 +0530

    Time: 0 ms (100%), Space: 57.6 MB (72.37%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    let firstArray=nums;
+    let secondArray=nums;
+    let result=firstArray.concat(secondArray)
+    return result;
+
+
+};
