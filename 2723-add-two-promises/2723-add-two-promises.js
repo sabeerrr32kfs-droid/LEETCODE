374f63e582621a615642a0af0d5a02d1fa5dea44
Mhammed Sabeer.k <sabeerrr32kfs@gmail.com>
2026-02-25 14:02:09 +0530

    Time: 53 ms (59.14%), Space: 53.9 MB (80.63%) - LeetHub

---
+++ 2723-add-two-promises/2723-add-two-promises.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    const value1=await promise1
+    const value2=await promise2
+    return value1 + value2
+};
+
